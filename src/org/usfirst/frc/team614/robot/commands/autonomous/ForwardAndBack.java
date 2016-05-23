eaf865ac09407c2e658e55b148c9094039477214
team614 <team614robotics@gmail.com>
2016-05-23 15:22:45 -0400

    functional mecanum baseline
    PWMS should match up with standard mecanum order

---
+++ src/org/usfirst/frc/team614/robot/commands/autonomous/ForwardAndBack.java
@@ -0,0 +1,15 @@
+
+package org.usfirst.frc.team614.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class ForwardAndBack extends CommandGroup {
+
+    public ForwardAndBack() {
+        // Use requires() here to declare subsystem dependencies
+       //requires(Robot.exampleSubsystem);
+    }
+}
--- src/org/usfirst/frc/team614/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team614/robot/subsystems/Drivetrain.java
@@ -16,42 +16,45 @@ public class Drivetrain extends Subsystem {
 
 	private final double SPEED_SCALE = 1.0;
 	
-	VictorSP frontleftMotor = new VictorSP(0); // 
-	VictorSP backleftMotor = new VictorSP(1);  //
-	VictorSP backrightMotor = new VictorSP(2); // 
-	VictorSP frontrightMotor = new VictorSP(3);// 
+	/*
+	 * RobotDrive enumerations for motors:
+		    static final int kFrontLeft_val = 0;
+		    static final int kFrontRight_val = 1;
+		    static final int kRearLeft_val = 2;
+		    static final int kRearRight_val = 3;
+	 */
+	
+//	 REVERSE N (MECANUM STANDARD):
+	VictorSP frontleftMotor = new VictorSP(0);
+	VictorSP backleftMotor = new VictorSP(1);
+	VictorSP frontrightMotor = new VictorSP(2);
+	VictorSP backrightMotor = new VictorSP(3);
+	
+//	 RobotDrive ENUMERATION:
+//	VictorSP frontleftMotor = new VictorSP(0);
+//	VictorSP backleftMotor = new VictorSP(2);
+//	VictorSP frontrightMotor = new VictorSP(1);
+//	VictorSP backrightMotor = new VictorSP(3);
+	
+//	U:
+//	VictorSP frontleftMotor = new VictorSP(0);
+//	VictorSP backleftMotor = new VictorSP(1);
+//	VictorSP frontrightMotor = new VictorSP(3);
+//	VictorSP backrightMotor = new VictorSP(2);
 	
 	RobotDrive drivetrain;
 	
 	    public Drivetrain() {
 	    	drivetrain = new RobotDrive(frontleftMotor, backleftMotor, frontrightMotor, backrightMotor);
-	        drivetrain.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+//	        drivetrain.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+//	        drivetrain.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
 	        drivetrain.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
-//	        drivetrain.setInvertedMotor(1, true);
-//	        drivetrain.setInvertedMotor(2, true);
-//	        drivetrain.setInvertedMotor(3, true);
+	        drivetrain.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
 	        
 	    }
-	    public void mecanumDriveMode(double magnitude, double direction, double rotation){ 	
-//	    	double controllerValue = 0.0;
-//	    	double magnitude = 0.0;
-//	    	double degrees = 0.0;
-//	    	double rotation = 0.0;
-//	    	
-//	    	controllerValue = controller.getMagnitude();
-//	    	
-//	    	if(controllerValue < Constants.JOYSTICK_DEADBAND && controllerValue > -Constants.JOYSTICK_DEADBAND)
-//	    		controllerValue = 0;	    	
-//	    	magnitude = -((1.0039215686275 * Math.pow(controllerValue, 3)) - (0.00616246498603 * controllerValue));
-//	    	
-//	    	degrees = controller.getDirectionDegrees();
-//	    	
-//	    	rotation = -(controller.getRawAxis(4));
-//	    	if(rotation < Constants.JOYSTICK_DEADBAND && rotation > -Constants.JOYSTICK_DEADBAND)
-//	    		rotation = 0;
-	    	//drivetrain.mecanumDrive_Cartesian(x, y, rotation, gyroAngle);
+	    public void mecanumDriveMode(double magnitude, double direction, double rotation) { 
 	    	drivetrain.mecanumDrive_Polar(magnitude / SPEED_SCALE, direction, rotation / SPEED_SCALE); //To increase the sensitivity, decrease SPEED_SCALE to a number below one but above 0
-	    																						//To decrease the sensitivity, increase SPEED_SCALE to a number above one; 		SPEED_SCALE is here until a proper ramp function is implemented
+																			//To decrease the sensitivity, increase SPEED_SCALE to a number above one; 		SPEED_SCALE is here until a proper ramp function is implemented
 	    }
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
